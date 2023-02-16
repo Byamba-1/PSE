e69d5d283515a5b67f0d81751550e0317e09f237
Byamba <byambaochir.oyungerel@gmail.com>
2023-02-16 14:48:50 +0100

    Fixed mistakes

---
+++ cmake-build-debug/CMakeFiles/PSE.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/byamba/CLionProjects/PSE/main.cpp" "CMakeFiles/PSE.dir/main.cpp.o" "gcc" "CMakeFiles/PSE.dir/main.cpp.o.d"
+  "/home/byamba/CLionProjects/PSE/parser/Byamba/CD.cpp" "CMakeFiles/PSE.dir/parser/Byamba/CD.cpp.o" "gcc" "CMakeFiles/PSE.dir/parser/Byamba/CD.cpp.o.d"
+  "/home/byamba/CLionProjects/PSE/parser/Byamba/tinystr.cpp" "CMakeFiles/PSE.dir/parser/Byamba/tinystr.cpp.o" "gcc" "CMakeFiles/PSE.dir/parser/Byamba/tinystr.cpp.o.d"
+  "/home/byamba/CLionProjects/PSE/parser/Byamba/tinyxml.cpp" "CMakeFiles/PSE.dir/parser/Byamba/tinyxml.cpp.o" "gcc" "CMakeFiles/PSE.dir/parser/Byamba/tinyxml.cpp.o.d"
+  "/home/byamba/CLionProjects/PSE/parser/Byamba/tinyxmlerror.cpp" "CMakeFiles/PSE.dir/parser/Byamba/tinyxmlerror.cpp.o" "gcc" "CMakeFiles/PSE.dir/parser/Byamba/tinyxmlerror.cpp.o.d"
+  "/home/byamba/CLionProjects/PSE/parser/Byamba/tinyxmlparser.cpp" "CMakeFiles/PSE.dir/parser/Byamba/tinyxmlparser.cpp.o" "gcc" "CMakeFiles/PSE.dir/parser/Byamba/tinyxmlparser.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PSE.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PSE.
+# This may be replaced when dependencies are built.
